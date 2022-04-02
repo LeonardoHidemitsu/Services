7ef01936afa8de5ff212cd78293485755a8bac14
Leoxdd100 <leonardohidemitsu@gmail.com>
2022-04-01 23:21:42 -0300

    1 file modified, 11 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { LogComponent } from './log/log.component';
 import { RouterModule } from '@angular/router';
 import { TimerService } from './timer.service';
-import { TasksService } from './tasks.service';
-import { LoggerService } from './logger.service';
 import { TemporizadorComponent } from './temporizador/temporizador.component';
 import { DisciplinasComponent } from './disciplinas/disciplinas.component';
 import { DisciplinasService } from './disciplinas.service';
@@ -20,9 +15,6 @@ import { DisciplinasService } from './disciplinas.service';
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'tasks', component: TasksComponent },
-      { path: 'log', component: LogComponent },
       { path: 'temporizador', component: TemporizadorComponent},
       { path: 'disciplinas', component: DisciplinasComponent},
     ]),
@@ -30,17 +22,12 @@ import { DisciplinasService } from './disciplinas.service';
   declarations: [
     AppComponent,
     HelloComponent,
-    HomeComponent,
-    TasksComponent,
-    LogComponent,
     TemporizadorComponent,
     DisciplinasComponent,
   ],
   bootstrap: [AppComponent],
   providers: [
     TimerService, 
-    TasksService,
-    LoggerService,
     DisciplinasService,
   ],
 })
